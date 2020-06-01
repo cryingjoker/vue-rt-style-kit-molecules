a4e1a0494e31d23b13aee5bf7946212b68c9de4d
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2020-06-02 00:17:40 +0300

    f7562 - удалить если не пригодится

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -14,25 +14,29 @@ const isLoadedCSS = document.body.className.indexOf('is-init-rt-molecules') !== 
  */
 const waitCSSByCond = ($el, propertyCss, needleValue) => {
   return new Promise((resolve, reject) => {
-    let call = 0
-    let maxCall = 40
-    let delayCall = 250
-    let checkCondition = () => {
-      return getComputedStyle($el)[propertyCss] === needleValue
-    }
-    if (checkCondition()) {
-      resolve()
+    if (typeof document !== "undefined") {
+      let call = 0
+      let maxCall = 40
+      let delayCall = 250
+      let checkCondition = () => {
+        return getComputedStyle($el) && getComputedStyle($el)[propertyCss] === needleValue
+      }
+      if (checkCondition()) {
+        resolve()
+      } else {
+        let timer = setInterval(() => {
+          if (checkCondition()) {
+            clearInterval(timer)
+            resolve()
+          }
+          if (++call >= maxCall) {
+            clearInterval(timer)
+            reject()
+          }
+        }, delayCall)
+      }
     } else {
-      let timer = setInterval(() => {
-        if (checkCondition()) {
-          clearInterval(timer)
-          resolve()
-        }
-        if (++call >= maxCall) {
-          clearInterval(timer)
-          reject()
-        }
-      }, delayCall)
+      resolve()
     }
   })
 }
