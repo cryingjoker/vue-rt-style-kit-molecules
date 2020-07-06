63e9df7b1c75b34b229117a289eaba61703c2d60
cryingjoker <cryingjoker@me.com>
2020-07-06 15:51:13 +0300

    add vertical space in slider

--- src/components/Slider/Slider.vue
+++ src/components/Slider/Slider.vue
@@ -5,13 +5,30 @@
   import SlideContent from "./SliderContent.vue";
   import SliderImage from "./SliderImage.vue";
   import debounce from "debounce";
+
   const componentList = [];
   componentList[SlideContent.name] = SlideContent;
   componentList[SliderImage.name] = SliderImage;
   export default {
     name: "RtSlider",
     props: {
-      activeIndex:{
+      activeIndex: {
+        type: Number,
+        default: 0
+      },
+      spaceVertical: {
+        type: Number,
+        default: 0
+      },
+      tdSpaceVertical: {
+        type: Number,
+        default: 0
+      },
+      mdSpaceVertical: {
+        type: Number,
+        default: 0
+      },
+      lgSpaceVertical: {
         type: Number,
         default: 0
       },
@@ -23,8 +40,8 @@
       customSlotsSort: [],
       maxImageHeight: 0,
       maxContentHeight: 0,
-      xDown:0,
-      yDown:0
+      xDown: 0,
+      yDown: 0
     }),
     created() {
       this.activeIndexLocal = this.activeIndex
@@ -52,7 +69,7 @@
       getSlotSort() {
         this.customSlotsSort = sliderStore.getSlotSort(this._uid);
       },
-      getMathHeight(){
+      getMathHeight() {
         this.getMaxContentHeight();
         this.getMaxImageHeight();
       },
@@ -80,18 +97,18 @@
           }, 100)
         }
       },
-      setNextSlide(){
-        this.activeIndexLocal = (this.activeIndexLocal+1)%this.customSlotsSort.length
+      setNextSlide() {
+        this.activeIndexLocal = (this.activeIndexLocal + 1) % this.customSlotsSort.length
       },
-      setPreviusSlide(){
-        this.activeIndexLocal = (this.activeIndexLocal-1+this.customSlotsSort.length)%this.customSlotsSort.length
+      setPreviusSlide() {
+        this.activeIndexLocal = (this.activeIndexLocal - 1 + this.customSlotsSort.length) % this.customSlotsSort.length
       },
-      touchstart(event){
+      touchstart(event) {
         this.xDown = event.touches[0].clientX;
         this.yDown = event.touches[0].clientY;
       },
-      touchmove(event){
-        if ( ! this.xDown || ! this.yDown ) {
+      touchmove(event) {
+        if (!this.xDown || !this.yDown) {
           return;
         }
 
@@ -101,8 +118,8 @@
         var xDiff = this.xDown - xUp;
         var yDiff = this.yDown - yUp;
 
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-          if ( xDiff > 0 ) {
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+          if (xDiff > 0) {
             this.setPreviusSlide()
           } else {
             this.setNextSlide()
@@ -113,10 +130,43 @@
         this.yDown = null;
       },
 
-
+      getSpacePartOfClass(space) {
+        const classList = ['sp', 'v'];
+        if (space == 0) {
+          classList.push('none')
+        } else {
+          classList.push(parseInt(space / 20) + '')
+          if (space % 20 > 0) {
+            classList.push(parseInt((space % 20) / 4) + '')
+          }
+        }
+        return classList.join('-')
+      }
 
     },
     computed: {
+      spaceClasses() {
+        const classList = ['slider', 'md-align-end'];
+        if (this.lgSpaceVertical) {
+          classList.push('lg-' + this.getSpacePartOfClass(this.lgSpaceVertical))
+        }
+        if (this.spaceVertical) {
+          classList.push(this.getSpacePartOfClass(this.spaceVertical))
+        } else {
+          classList.push('sp-v-4')
+        }
+        if (this.tdSpaceVertical) {
+          classList.push('td-' + this.getSpacePartOfClass(this.tdSpaceVertical))
+        } else {
+          classList.push('td-sp-v-3')
+        }
+        if (this.mdSpaceVertical) {
+          classList.push('md-' + this.getSpacePartOfClass(this.mdSpaceVertical))
+        } else {
+          classList.push('md-sp-v-2')
+        }
+        return classList.join(' ')
+      },
       contentStyle() {
         let style = ''
         if (this.maxContentHeight > 0) {
@@ -137,7 +187,8 @@
     render(h) {
 
 
-      return <rt-row class="slider md-align-end" mobile-reverse-direction={true} ref="slider" onTouchstart={this.touchstart} onTouchmove={this.touchmove}>
+      return <rt-row class={this.spaceClasses} mobile-reverse-direction={true} ref="slider"
+                     onTouchstart={this.touchstart} onTouchmove={this.touchmove}>
 
         {this.$slots.default}
         <rt-col size="1" tablet-size="0" mobile-size="0"></rt-col>
@@ -153,7 +204,8 @@
             <div class="slider-arrow d-flex sp-t-2-2 td-sp-t-2 md-sp-t-1-3">
               <div class="slider-arrow-icon slider-arrow-icon__prev" onClick={this.setPreviusSlide}>
 
-                <svg class="slider-arrow-svg" width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <svg class="slider-arrow-svg" width="9px" height="16px" viewBox="0 0 9 16" version="1.1"
+                     xmlns="http://www.w3.org/2000/svg">
                   <g transform="translate(30, -18)">
                     <polygon id="Path" fill="#101828" fill-rule="nonzero"
                              transform="scale(-1, 1)"
@@ -161,8 +213,9 @@
                   </g>
                 </svg>
               </div>
-              <div class="slider-arrow-icon slider-arrow-icon__next"  onClick={this.setNextSlide}>
-                <svg class="slider-arrow-svg" width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <div class="slider-arrow-icon slider-arrow-icon__next" onClick={this.setNextSlide}>
+                <svg class="slider-arrow-svg" width="9px" height="16px" viewBox="0 0 9 16" version="1.1"
+                     xmlns="http://www.w3.org/2000/svg">
                   <g transform="translate(-21, -18.000000)">
                     <polygon id="Path" fill="#101828" fill-rule="nonzero"
                              points="21 19.0586108 22.0460529 18 30 26 22.0460529 34 21 32.9413892 27.9009314 25.9993262"></polygon>
@@ -175,11 +228,11 @@
         <rt-col size="6" tablet-size="3" mobile-size="2" class="md-sp-b-1-1">
           <div class="slider-image" style={this.imageStyle}>
             <div class="relative">
-            {this.customSlotsSort.map((key, index) => {
-              return <rt-slider-image index={index}
-                                      active-index={this.activeIndexLocal}
-                                      data={this.customSlots[key]}></rt-slider-image>
-            })}
+              {this.customSlotsSort.map((key, index) => {
+                return <rt-slider-image index={index}
+                                        active-index={this.activeIndexLocal}
+                                        data={this.customSlots[key]}></rt-slider-image>
+              })}
             </div>
           </div>
         </rt-col>
