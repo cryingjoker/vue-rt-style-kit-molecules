856c31bc18ad15fbaeecc22ff3add913dfe8e33c
wikked_one <wikked616@gmail.com>
2020-08-31 12:57:04 +0300

    phone-number input added client autocomplete & cut off rt-select

--- src/components/PhoneNumberInput/PhoneNumberInput.vue
+++ src/components/PhoneNumberInput/PhoneNumberInput.vue
@@ -24,6 +24,10 @@
       clearButtonText: {
         type: String,
         default: 'Очистить фильтр'
+      },
+      clientAutoComplete: {
+        type: String,
+        default: ''
       }
     },
     data(){
@@ -36,17 +40,28 @@
         startVal: '',
         nowVal: '',
         startCode: '',
-        areaCodeLength: null,
+        areaCodeLength: 3,
         selectWasInteracted: 0,
-        areaCodeLocal: []
+        areaCodeLocal: [],
+        selectedValue: null,
+        selectOpened: false,
+        localClientAutoComplete: [],
+        stickedAutoComplete: [],
+        stickedStringAutoComplete: ''
       }
     },
     watch: {
       autoComplete(newVal, oldVal) {
+        this.localAutoComplete = newVal.split('');
         this.countStartVal();
       },
       areaCode(newVal, oldVal) {
         this.areaCodeLocal = newVal;
+        this.setStartCodeValue();
+        this.countStartVal();
+      },
+      clientAutoComplete(newVal, oldVal) {
+        this.localClientAutoComplete = newVal.split('');
         this.countStartVal();
       }
     },
@@ -57,44 +72,75 @@
           classList += ' rt-phone-input__prefix--separated'
         }
         return classList;
+      },
+      selectClasses() {
+        let classList = "rt-phone-input__select";
+        if(this.selectOpened) {
+          classList += " rt-phone-input__select--active";
+        }
+        return classList;
       }
     },
     mounted(){
       this.localVal = this.autoComplete;
       this.areaCodeLocal = this.areaCode;
-
+      this.localAutoComplete = this.localVal.split('');
+      this.localClientAutoComplete = this.clientAutoComplete.split('');
+      this.setStartCodeValue();
       this.countStartVal();
+      setTimeout(() => {
+        if(this.$refs.inputsWrapper.children.length) {
+          this.setFocus();
+        }
+      },0)
     },
     methods: {
       countStartVal(){
         if(this.areaCodeLocal.length) {
-          if(!isNaN(this.areaCodeLocal[0].value)) {
-            this.areaCodeLength = this.areaCodeLocal[0].value.toString().length;
-          } else {
-            this.areaCodeLength = this.areaCodeLocal[1].value.toString().length;
-          }
           this.inputsLength = 11 - this.prefix.toString().length - this.areaCodeLength;
         }
-        this.localAutoComplete = this.localVal.split('');
         this.localAutoComplete.map((item, index) => {
           item == '\\' ? this.localAutoComplete.splice(index, 1) : false;
         });
+        this.localClientAutoComplete.map((item, index) => {
+            item == '\\' ? this.localClientAutoComplete.splice(index, 1) : false;
+        });
+        this.stickedAutoComplete.length = this.localAutoComplete.length;
+        for(let i = 0; i < this.stickedAutoComplete.length; i++) {
+            if(isNaN(this.localAutoComplete[i])) {
+                this.stickedAutoComplete[i] = {"value": this.localClientAutoComplete[i], "disabled": false};
+            } else {
+                this.stickedAutoComplete[i] = {"value": this.localAutoComplete[i], "disabled": true};
+            }
+        }
+        this.stickedAutoComplete.map((item) => {
+          if(item.value == 'd') {
+            item.value = '\\d'
+          }
+        });
+        this.stickedAutoComplete.map((item) => {
+            this.stickedStringAutoComplete += item.value;
+        })
         this.startVal = this.prefix.toString();
         if(this.areaCodeLocal.length) {
           if(this.areaCodeLocal.length > 1) {
-            if(isNaN(this.areaCodeLocal[0].value)) {
-              for(let i = 0; i < this.areaCodeLength; i++) {
-                this.startVal += '\\d';
+            this.areaCodeLocal.map((item) => {
+              if(item.preselected) {
+                if(isNaN(item.value)) {
+                  for(let i = 0; i < this.areaCodeLength; i++) {
+                    this.startVal += '\\d';
+                  }
+                } else {
+                  this.startVal += item.value;
+                }
               }
-            } else {
-              this.startVal += this.areaCodeLocal[0].value;
-            }
+            })
           } else {
             this.startVal += this.areaCodeLocal[0].value;
           }
         }
         if(this.autoComplete) {
-          this.startVal += this.autoComplete;
+          this.startVal += this.stickedStringAutoComplete;
         } else {
           for(let i = 0; i < this.inputsLength; i++) {
             this.startVal += '\\d';
@@ -137,7 +183,7 @@
       },
       setFocus() {
         this.targetIndex = this.countActive('forward', -1);
-        this.$refs.inputsWrapper.children[this.targetIndex].focus();
+        this.$refs.inputsWrapper.children.length ? this.$refs.inputsWrapper.children[this.targetIndex].focus() : false;
         this.stickNumber();
         this.selectWasInteracted++;
         if(this.selectWasInteracted > 1) {
@@ -169,68 +215,93 @@
       stickNumber() {
         this.nowVal = this.prefix;
         if(this.$refs.dropdown) {
-          if(!isNaN(this.$refs.dropdown.localValue)) {
-            this.nowVal += this.$refs.dropdown.localValue;
+          if(!isNaN(this.selectedValue)) {
+            this.nowVal += this.selectedValue.toString();
           } else {
             for(let i = 0; i < this.areaCodeLength; i++) {
               this.nowVal += '\\d';
             }
           }
         } else {
-          this.nowVal += this.$refs.preselected.innerText;
+          this.nowVal += this.selectedValue.toString();
         }
         for(let i = 0; i < this.inputsLength; i++) {
           if(this.$refs['input-' + i]) {
             if(this.$refs['input-' + i].value == '') {
-                this.nowVal += '\\d';
+              this.nowVal += '\\d';
             } else {
-                this.nowVal += this.$refs['input-' + i].value;
+              this.nowVal += this.$refs['input-' + i].value;
             }
           }
         }
         this.eraseButton = this.startVal !== this.nowVal;
-        if(this.wasInteracted > 0) {
+        this.$emit('selected-number', this.nowVal);
+      },
+      emitOrder() {
+        if(this.nowVal) {
           this.$emit('selected-number', this.nowVal);
         }
       },
-      emitOrder() {
-        this.$emit('selected-number', this.nowVal)
-      },
       clearData() {
-        this.$refs.dropdown ? this.$refs.preselected.setSelection() : false;
+        this.$refs.dropdown ? this.selectedValue = this.startCode : false;
         for(let i = 0; i < this.inputsLength; i++) {
           !this.$refs['input-' + i].hasAttribute('disabled') ? this.$refs['input-' + i].value = '' : false;
         }
         this.stickNumber();
         this.$emit('search-cleared', this.nowVal)
       },
+      open() {
+        this.selectOpened = !this.selectOpened;
+      },
+      setStartCodeValue() {
+        this.areaCodeLocal.map((item) => {
+          if(item.preselected) {
+           this.startCode = item.code;
+           this.selectedValue = item.code;
+          }
+        })
+      }
     },
     render: function(h) {
       const codeDropdown = () => {
         const selectOptions = () => {
-          return this.areaCodeLocal.map((item, index) => {
-            if(index == 0) {
-              return <rt-select-option selected={true} value={item.value.toString()} ref="preselected">{item.code}</rt-select-option>
+          return this.areaCodeLocal.map((item) => {
+            const setValue = () => {
+              this.selectedValue = item.code;
+              this.setFocus();
+            };
+            if(item.preselected) {
+              return <div class="rt-phone-input__select-item rt-phone-input__select-item--selected"
+                          selected={true} value={item.value.toString()} ref="preselected" onClick={setValue}>{item.code}</div>
             } else {
-              return <rt-select-option value={item.value.toString()}>{item.code}</rt-select-option>
+              return <div class="rt-phone-input__select-item" value={item.value.toString()} onClick={setValue}>{item.code}</div>
             }
           })
         };
         if(this.areaCodeLocal.length) {
           if(this.areaCodeLocal.length > 1) {
-            return <rt-select ref="dropdown" onInput={this.setFocus} onChange={this.stickNumber}>
-              {selectOptions()}
-            </rt-select>
+            return <div class={this.selectClasses} ref="dropdown" onClick={this.open}>
+              <span>{this.selectedValue}</span>
+              <div class="rt-phone-input__select-items-wrapper">
+                {selectOptions()}
+              </div>
+            </div>
           } else {
-            return <p class="single-code" ref="preselected" value={this.areaCode[0].value.toString()}>{this.areaCode[0].code}</p>
+            return <p class="single-code" ref="preselected">{this.areaCode[0].code}</p>
           }
         }
       };
       const inputs = () => {
         for(let i = 0; i < this.inputsLength; i++) {
-          return this.localAutoComplete.map((item, index) => {
-            if(!isNaN(item)) {
-              return <input class="rt-phone-input__single-one" value={item} disabled ref={'input-' + index}>{item}</input>
+          return this.stickedAutoComplete.map((item, index) => {
+            if(!isNaN(item.value)) {
+              if(item.disabled) {
+                return <input class="rt-phone-input__single-one" value={item.value} disabled ref={'input-' + index}>{item.value}</input>
+              } else {
+                return <input class="rt-phone-input__single-one" value={item.value} ref={'input-' + index}
+                              onFocus={this.setCursor} type="number" onBlur={this.returnPlaceholder}
+                              onKeyup={this.moveFocus} onKeydown={this.changeValue} onInput={this.stickNumber}>{item.value}</input>
+              }
             } else {
               return <input class="rt-phone-input__single-one"
                             onFocus={this.setCursor} type="number" onBlur={this.returnPlaceholder}
