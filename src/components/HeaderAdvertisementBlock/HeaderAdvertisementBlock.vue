e8688cfed30d94793bbf441d39805435494398e7
wikked_one <wikked616@gmail.com>
2020-10-15 20:27:25 +0300

    location losing bug fix in navigation

--- src/components/HeaderAdvertisementBlock/HeaderAdvertisementBlock.vue
+++ src/components/HeaderAdvertisementBlock/HeaderAdvertisementBlock.vue
@@ -27,37 +27,22 @@
     mounted(){},
     computed: {},
     methods: {
-      fixRegion($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        let urlString = window.location.href;
-        let startPos = urlString.indexOf('/-');
-        let endPos = urlString.indexOf('-/');
-        let place = '';
-        if(startPos != -1) {
-          if(endPos != -1) {
-            place = `-${urlString.substring(startPos + 2, endPos)}-`
-          } else {
-            place = `-${urlString.substring(startPos + 2, urlString.length - 1)}-`
-          }
-        }
-        window.location.href = urlString.substring(0, startPos) + place + this.linkTarget;
-      }
+
     },
     render(h) {
       return <div class="rt-header__promo-block">
-        <a href={this.linkTarget} class="rt-header__ab-image-wrapper" onClick={this.fixRegion}>
+        <a href={this.linkTarget} class="rt-header__ab-image-wrapper">
           <img src={this.image} class="rt-header__ab-image"/>
         </a>
         <div class="rt-header__ab-content-block">
           <div class="rt-header__ab-content-block-top">
-            <a href={this.linkTarget} onClick={this.fixRegion}>
+            <a href={this.linkTarget}>
               <h5 class="p1 rt-font-bold">{this.$slots.title}</h5>
             </a>
             <p class="p3 color-main07">{this.$slots.paragraph}</p>
           </div>
           <div class="rt-header__ab-content-block-bottom">
-            <a href={this.linkTarget} class="p3 rt-link rt-link--purple" onClick={this.fixRegion}>{this.linkText}</a>
+            <a href={this.linkTarget} class="p3 rt-link rt-link--purple">{this.linkText}</a>
           </div>
         </div>
       </div>
