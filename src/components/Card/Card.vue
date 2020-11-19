cd6cd5b7e34089ddb5db30238bb1f0c0b9225a53
Mikhail.Malyuga <Mikhail.Malyuga@south.rt.ru>
2020-11-20 00:51:41 +0300

    RTRUB2B-4717 new Event в Card.vue для IE

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -544,7 +544,16 @@
         if (this.inTabsWImage)
           this.redrawSvg();
       })
-      window.dispatchEvent(new Event('resize'));
+      if (typeof(Event) === 'function') {
+          // modern browsers
+          window.dispatchEvent(new Event('resize'));
+      } else {
+          // for IE and other old browsers
+          // causes deprecation warning on modern browsers
+          var evt = window.document.createEvent('UIEvents'); 
+          evt.initUIEvent('resize', true, false, window, 0); 
+          window.dispatchEvent(evt);
+      }
 
       if (this.inTabsWImage)
         this.redrawSvg();
