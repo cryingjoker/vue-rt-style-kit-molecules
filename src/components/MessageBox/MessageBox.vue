19e7adda744682d339525c9883f22259c5f7ba12
Sergey.Sarkisyants <Sergey.Sarkisyants@south.rt.ru>
2021-02-01 11:00:36 +0300

    RTRUB2B-4868 Move content to slot, change class naming

--- src/components/MessageBox/MessageBox.vue
+++ src/components/MessageBox/MessageBox.vue
@@ -10,14 +10,6 @@ export default {
     }
   },
   props: {
-    title: {
-      type: String,
-      default: 'Не нашли, что искали?'
-    },
-    subtitle: {
-      type: String,
-      default: 'Больше информации и предложений на нашем сайте'
-    },
     buttonText: {
       type: String,
       default: 'Перейти'
@@ -58,8 +50,8 @@ export default {
     },
     parentClasses() {
       return 'rt-message-box' +
-        (this.isLightTheme ? ' rt-message-box__theme-light background-white color-main' : ' rt-message-box__theme-dark background-main-color color-white') +
-        (this.show ? ' rt-message-box__show' : '') +
+        (this.isLightTheme ? ' rt-message-box--is-theme-light background-white color-main' : ' rt-message-box--is-theme-dark background-main-color color-white') +
+        (this.show ? ' rt-message-box--is-show' : '') +
         ' sp-v-0-3 sp-h-0-4 td-sp-v-1 td-sp-h-1 md-sp-v-1-1'
     },
     closeIconColor() {
@@ -100,21 +92,21 @@ export default {
     return <div class={this.parentClasses}>
       <div class="td-d-flex td-flex-v-end td-space-between">
         <div>
-          <p class="rt-font-small-paragraph rt-font-bold" domPropsInnerHTML={this.title}></p>
-          <p class="rt-font-control sp-t-0-2 md-sp-t-0-1" domPropsInnerHTML={this.subtitle}></p>
+          {this.$slots.default}
         </div>
         <a onClick={this.action} class="d-inline-block flex-fill sp-t-0-3 td-sp-t-none td-sp-l-1-2 md-sp-l-1">
-          <rt-button small={true} color={this.buttonColor}
-                     bright={this.isDarkTheme}>{this.buttonText}</rt-button>
+          <rt-button small={true} color={this.buttonColor} bright={this.isDarkTheme}>
+            {this.buttonText}
+          </rt-button>
         </a>
       </div>
 
-      <button class="rt-message-box-close flex-center-center rt-sys-icon-hover--orange" onClick={this.close}>
+      <button class="rt-message-box__close flex-center-center rt-sys-icon-hover--orange" onClick={this.close}>
         <rt-system-icons name="close large" color={this.closeIconColor} />
       </button>
 
       <svg width="107" height="92" viewBox="0 0 107 92" xmlns="http://www.w3.org/2000/svg"
-           class="rt-message-box-corner">
+           class="rt-message-box__corner">
         <path d="M107 33 V0 C106 9 105 39 66 60 C90 48 99 46 107 33 Z" fill={this.segmentColor} />
         <path d="M65 60 L0 92 L107 92 V32 C103 44 78 53 65 60 Z" fill="#FF4F12" />
       </svg>
