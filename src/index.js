8ce373ff75de6dc587a9aa97af8139597d7b3717
Mikhail.Malyuga <malyugami@gmail.com>
2021-06-04 14:00:35 +0300

    build

--- src/index.js
+++ src/index.js
@@ -275,7 +275,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-05-21 15:40";
+const version = "2021-06-04 13:56";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
