84ce745a82c37ac0299d585ccbe03092146397f0
andrey.margushin <andrey.margushin@south.rt.ru>
2021-06-30 08:57:18 +0300

    RTRUB2B-5350 build test

--- src/index.js
+++ src/index.js
@@ -275,7 +275,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-06-29 14:00";
+const version = "2021-06-30 08:55";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
