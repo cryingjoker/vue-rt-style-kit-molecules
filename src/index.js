90ba9f49f119bf461e20af191b269d0bf62803f0
vitaliy.ivankov <vitaliy.ivankov@south.rt.ru>
2021-07-05 19:42:20 +0300

    build

--- src/index.js
+++ src/index.js
@@ -275,7 +275,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-07-01 10:36";
+const version = "2021-07-05 19:41";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
