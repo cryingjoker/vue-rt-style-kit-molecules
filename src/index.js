0e7fc40b5e8b507aae5178117fcf92a1e81e6b23
cryingjoker <cryingjoker@me.com>
2021-07-07 16:13:28 +0300

    build

--- src/index.js
+++ src/index.js
@@ -275,7 +275,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-07-07 16:09";
+const version = "2021-07-07 16:12";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
