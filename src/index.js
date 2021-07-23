fcf6c901d4b8033448eb7072e1870038f96484b0
andrey.margushin <andrey.margushin@south.rt.ru>
2021-07-23 13:17:13 +0300

    RTRUB2B-5448 Callback, требуется убрать лишний пуш

--- src/index.js
+++ src/index.js
@@ -277,7 +277,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-07-20 16:18";
+const version = "2021-07-23 13:15";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
