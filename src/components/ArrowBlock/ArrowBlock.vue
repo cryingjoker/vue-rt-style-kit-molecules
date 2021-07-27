b75be243d6fc2750495156c0a7c315dd06b9e12d
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2021-07-27 18:03:49 +0300

    RTRU-11077 - Вынес тело компонента в рендер для отображения иконок

--- src/components/ArrowBlock/ArrowBlock.vue
+++ src/components/ArrowBlock/ArrowBlock.vue
@@ -15,41 +15,31 @@ export default {
   beforeDestroy: function () {
   },
   methods: {},
-  computed: {
-    renderIcon() {
-      if (this.$slots.icon) {
-        return <div class="rt-arrow-block__icon sp-r-1-2 md-sp-r-1">{this.$slots.icon}</div>
-      }
-      return null
-    },
-  },
   render(h) {
+    const renderIcon = this.$slots.icon
+      ? <div class="rt-arrow-block__icon sp-r-1-2 md-sp-r-1">{this.$slots.icon}</div>
+      : null
+    const renderBody = <div class="rt-arrow-block__body d-inline-block">
+      <div class="rt-arrow-block__label rt-font-h4">
+        {this.$slots.label}
+      </div>
+      <div class="rt-arrow-block__content sp-t-0-3 sp-r-1-1">
+        {this.$slots.content}
+      </div>
+    </div>
+
     if(this.href.length > 0) {
       return <a href={this.href} class="rt-arrow-block sp-v-1-3 sp-l-1-3 sp-r-2-3 md-sp-h-1 rt-sys-icon-hover--orange">
-        {this.renderIcon}
-        <div class="rt-arrow-block__body d-inline-block">
-          <div class="rt-arrow-block__label rt-font-h4">
-            {this.$slots.label}
-          </div>
-          <div class="rt-arrow-block__content sp-t-0-3 sp-r-1-1">
-            {this.$slots.content}
-          </div>
-        </div>
+        {renderIcon}
+        {renderBody}
         <div class="rt-arrow-block__arrow">
           <rt-system-icons name="arrow right"></rt-system-icons>
         </div>
       </a>
     }
     return <div class="rt-arrow-block sp-v-1-3 sp-l-1-3 sp-r-2-3 md-sp-h-1 rt-sys-icon-hover--orange">
-      {this.renderIcon}
-      <div class="rt-arrow-block__body d-inline-block">
-        <div class="rt-arrow-block__label rt-font-h4">
-          {this.$slots.label}
-        </div>
-        <div class="rt-arrow-block__content sp-t-0-3 sp-r-1-1">
-          {this.$slots.content}
-        </div>
-      </div>
+      {renderIcon}
+      {renderBody}
       <div class="rt-arrow-block__arrow">
         <rt-system-icons name="arrow right"></rt-system-icons>
       </div>
