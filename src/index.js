2ce219ade293c3d0a9c2b15928fbc902b05142fb
Viktor.Sidorets <viktor.sidorets@south.rt.ru>
2021-08-02 14:30:01 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -277,7 +277,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-07-23 14:51";
+const version = "2021-08-02 14:28";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
