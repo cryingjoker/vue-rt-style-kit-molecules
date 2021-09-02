6f344689d5aa48e41fb78a0069184bbce637ffaa
Ратьков Сергей <sergey.ratkov@rt.ru>
2021-09-02 16:25:50 +0300

    build

--- src/index.js
+++ src/index.js
@@ -273,7 +273,7 @@ const VueRtStyle = {
  * с названием из Global.globalSettingsKey и положите перед подключением либы
  */
 const settingsKey = Global.globalSettingsKey;
-const version = "2021-09-02 15:34";
+const version = "2021-09-02 16:25";
 if (typeof window !== 'undefined') {
 
   if (settingsKey) {
