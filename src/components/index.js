c46cede50a01981a2610c6f3b59f613887424de7
wikked616 <wikked616@gmail.com>
2021-09-21 12:07:27 +0300

    dynamic molecules

--- src/components/index.js
+++ src/components/index.js
@@ -22,6 +22,22 @@ export {default as CarouselV2} from './CarouselV2/Carousel.vue';
 export {default as CarouselSlideV2} from './CarouselV2/CarouselSlide.vue';
 export {default as CarouselV3} from './CarouselV3/CarouselV3.vue';
 export {default as CarouselV3Item} from './CarouselV3/CarouselV3Item.vue';
+export {default as CheckboxTabs} from './CheckboxTabs/CheckboxTabs.vue';
+export {default as CommentSlider} from './CommentSlider/CommentSlider.vue';
+export {default as CommentSliderItem} from './CommentSlider/CommentSliderItem.vue';
+export {default as CountdownTimer} from './CountdownTimer/CountdownTimer.vue';
+export {default as DownloadContent} from './DownloadContent/DownloadContent.vue';
+export {default as HelpBlock} from './HelpBlock/HelpBlock.vue';
+export {default as Jumbotron} from './Jumbotron/Jumbotron.vue';
+export {default as MessageBox} from './MessageBox/MessageBox.vue'
+export {default as Microtabs} from './Microtabs/Microtabs.vue';
+export {default as MicrotabsContent} from './Microtabs/MicrotabsContent.vue';
+export {default as MicrotabsNavigation} from './Microtabs/MicrotabsNavigation.vue';
+export {default as UnWrapper} from './MobileExpandWrapper/UnWrapper.vue';
+export {default as OptionsList} from './OptionsList/OptionsList.vue';
+export {default as OptionsListInfoItem} from './OptionsList/OptionsListInfoItem.vue';
+export {default as PhoneNumberInput} from './PhoneNumberInput/PhoneNumberInput.vue';
+export {default as Popover} from './Popover/Popover.vue';
 
 
 export {default as ProductCard} from './ProductCard/ProductCard.vue';
@@ -29,72 +45,55 @@ export {default as ScrollTabs} from './ScrollTabs/ScrollTabs.vue';
 export {default as Tabs} from './Tabs/Tabs.vue';
 export {default as TabsSlider} from './TabsSlider/TabsSlider.vue';
 export {default as TabsSliderItem} from './TabsSlider/TabsSliderItem.vue';
-export {default as CheckboxTabs} from './CheckboxTabs/CheckboxTabs.vue';
 export {default as TabsNavigationItem} from './Tabs/TabsNavigationItem.vue';
 export {default as TabsContentItem} from './Tabs/TabsContentItem.vue';
-export {default as Microtabs} from './Microtabs/Microtabs.vue';
-export {default as MicrotabsContent} from './Microtabs/MicrotabsContent.vue';
-export {default as MicrotabsNavigation} from './Microtabs/MicrotabsNavigation.vue';
 export {default as RowList} from './RowList/RowList.vue';
-export {default as OptionsList} from './OptionsList/OptionsList.vue';
-export {default as OptionsListInfoItem} from './OptionsList/OptionsListInfoItem.vue';
 export {default as RowListItem} from './RowList/RowListItem.vue';
 export {default as Table} from './Table/Table.vue';
 export {default as TableHeadItem} from './Table/TableHeadItem.vue';
 export {default as TableItem} from './Table/TableItem.vue';
 export {default as TableCol} from './Table/TableCol.vue';
 export {default as TableRow} from './Table/TableRow.vue';
-export {default as DownloadContent} from './DownloadContent/DownloadContent.vue';
-export {default as InlineDropdown} from './InlineDropdown/InlineDropdown.vue';
-export {default as InlineDropdownItem} from './InlineDropdown/InlineDropdownItem.vue';
-export {default as Tag} from './Tag/Tag.vue';
-export {default as TagsList} from './Tag/TagsList.vue';
-export {default as Filter} from './Filter/Filter.vue';
-export {default as FilterWatcher} from './Filter/FilterWatcher.vue';
-export {default as FilterCaller} from './Filter/FilterCaller.vue';
 export {default as ShowOn} from './ShowOn/ShowOn.vue';
-export {default as Popover} from './Popover/Popover.vue';
-export {default as FilterV2} from './FilterV2/Filter.vue';
-export {default as FilterItemV2} from './FilterV2/FilterItem.vue';
-export {default as FilterSetterV2} from './FilterV2/FilterSetter.vue';
 export {default as Youtube} from './Youtube/Youtube.vue';
 export {default as Sticky} from './Sticky/Sticky.vue';
 export {default as ResizeContentHeight} from './ResizeContentHeight/ResizeContentHeight.vue';
-export {default as SlideContent} from './SlideContent/SlideContent.vue';
-export {default as SlideContentDottedContent} from './SlideContent/SlideContentDottedContent.vue';
-export {default as SlideContentDottedHeader} from './SlideContent/SlideContentDottedHeader.vue';
 export {default as Popup} from './Popup/Popup.vue';
 export {default as StickyHeaderLine} from './StickyHeaderLine/StickyHeaderLine.vue';
-export { default as LinksBlock } from './Card/LinksBlock.vue';
-export { default as Pattern } from './Pattern/Pattern.vue';
-export { default as Swiper } from './Swiper/Swiper.vue';
-export { default as Slide } from './Swiper/Slide.vue';
-export { default as Opportunity } from './Opportunity/Opportunity.vue';
-export { default as OpportunityItem } from './Opportunity/OpportunityItem.vue';
-export { default as UnWrapper } from './MobileExpandWrapper/UnWrapper.vue';
-export { default as RealPopup } from './Popup/RealPopup.vue';
-export { default as ScrollBar } from './ScrollBar/ScrollBar.vue';
-export { default as VerticalScrollBar } from './ScrollBar/VerticalScrollBar.vue';
-export { default as ReplacementLink } from './ReplacementLinkWrapper/ReplacementLink.vue';
-export { default as GameArrow } from './Game/Arrow/GameArrow.vue'
-export { default as GalleryCarousel } from './GalleryCarousel/GalleryCarousel.vue';
-export { default as GalleryCarouselItem } from './GalleryCarousel/GalleryCarouselItem.vue';
-export { default as CountdownTimer } from './CountdownTimer/CountdownTimer.vue';
-export { default as FunctionItem } from './FunctionList/FunctionItem.vue';
-export { default as FunctionList } from './FunctionList/FunctionList.vue';
-export { default as TextImageBlock } from './TextImageBlock/TextImageBlock.vue';
-export { ResizeTypeStore } from './ResizeTypeStore/ResizeTypeStore.js';
+export {default as RealPopup} from './Popup/RealPopup.vue';
+export {default as ReplacementLink} from './ReplacementLinkWrapper/ReplacementLink.vue';
+export {default as TextImageBlock} from './TextImageBlock/TextImageBlock.vue';
 export {default as Slider} from './Slider/Slider.vue'
 export {default as SliderItem} from './Slider/SliderItem.vue'
 export {default as StickyBottomLine} from './StickyBottomLine/StickyBottomLine.vue';
-export {default as PhoneNumberInput} from './PhoneNumberInput/PhoneNumberInput.vue';
 export {default as PreviewBanner} from './PreviewBanner/PreviewBanner.vue';
-export {default as CommentSlider} from './CommentSlider/CommentSlider.vue';
-export {default as CommentSliderItem} from './CommentSlider/CommentSliderItem.vue';
-
-
-export {default as Jumbotron} from './Jumbotron/Jumbotron.vue';
-export {default as HelpBlock} from './HelpBlock/HelpBlock.vue';
-export {default as MessageBox} from './MessageBox/MessageBox.vue'
 export {default as VideoBanner} from './VideoBanner/VideoBanner.vue';
 export {default as Privilege} from './Privilege/Privilege.vue';
+export { ResizeTypeStore } from './ResizeTypeStore/ResizeTypeStore.js';
+
+
+// delete?
+export {default as LinksBlock} from './Card/LinksBlock.vue';
+export {default as Filter} from './Filter/Filter.vue';
+export {default as FilterCaller} from './Filter/FilterCaller.vue';
+export {default as FilterWatcher} from './Filter/FilterWatcher.vue';
+export {default as FilterV2} from './FilterV2/Filter.vue';
+export {default as FilterItemV2} from './FilterV2/FilterItem.vue';
+export {default as FilterSetterV2} from './FilterV2/FilterSetter.vue';
+export {default as FunctionList} from './FunctionList/FunctionList.vue';
+export {default as FunctionItem} from './FunctionList/FunctionItem.vue';
+export {default as InlineDropdown} from './InlineDropdown/InlineDropdown.vue';
+export {default as InlineDropdownItem} from './InlineDropdown/InlineDropdownItem.vue';
+export {default as Pattern} from './Pattern/Pattern.vue';
+export {default as ScrollBar} from './ScrollBar/ScrollBar.vue';
+export {default as VerticalScrollBar} from './ScrollBar/VerticalScrollBar.vue';
+export {default as Swiper} from './Swiper/Swiper.vue';
+export {default as Slide} from './Swiper/Slide.vue';
+export {default as SlideContent} from './SlideContent/SlideContent.vue';
+export {default as SlideContentDottedContent} from './SlideContent/SlideContentDottedContent.vue';
+export {default as SlideContentDottedHeader} from './SlideContent/SlideContentDottedHeader.vue';
+export {default as Tag} from './Tag/Tag.vue';
+export {default as TagsList} from './Tag/TagsList.vue';
+
+//game-arrow, gallery-carousel, opportunity deleted
+
